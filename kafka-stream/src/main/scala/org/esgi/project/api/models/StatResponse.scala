d403e740a10f6b0082122e7e0a1d42d8645a1fc2
NoobZik <noobzik@pm.me>
2022-04-14 16:08:38 +0200

    Fixed type

--- kafka-stream/src/main/scala/org/esgi/project/api/models/StatResponse.scala
+++ kafka-stream/src/main/scala/org/esgi/project/api/models/StatResponse.scala
@@ -3,9 +3,9 @@ package org.esgi.project.api.models
 import play.api.libs.json.{Json, OFormat}
 
 case class Stat(
-                 start_only: String,
-                 half : String,
-                 full: String
+                 start_only: Long,
+                 half : Long,
+                 full: Long
                )
 
 object StatResponse {
