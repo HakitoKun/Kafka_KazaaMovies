3211b8002c15670182bb971cd9f70039ab160ec0
NoobZik <noobzik@pm.me>
2022-04-14 10:31:36 +0200

    Pushed models

--- kafka-stream/src/main/scala/org/esgi/project/streaming/models/Likes.scala
+++ kafka-stream/src/main/scala/org/esgi/project/streaming/models/Likes.scala
@@ -2,21 +2,16 @@ package org.esgi.project.streaming.models
 
 import play.api.libs.json.{Json, OFormat}
 
-case class MeanLatencyForURL(
-                              sum: Long,
-                              count: Long,
-                              meanLatency: Long
-                            ) {
-  def increment(latency: Long) = this.copy(sum = this.sum + latency, count = this.count + 1)
+case class Likes(
+                              _id: Float,
+                              score: Float,
+                            )
 
-  def computeMeanLatency = this.copy(
-    meanLatency = this.sum / this.count
-  )
+
+object Likes {
+  implicit val format: OFormat[Likes] = Json.format[Likes]
 }
 
 
-object MeanLatencyForURL {
-  implicit val format: OFormat[MeanLatencyForURL] = Json.format[MeanLatencyForURL]
 
-  def empty: MeanLatencyForURL = MeanLatencyForURL(0, 0, 0)
-}
+
