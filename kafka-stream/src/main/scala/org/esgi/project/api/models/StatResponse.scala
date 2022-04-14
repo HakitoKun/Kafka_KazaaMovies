2bf7a0fd2327e35984e3c646e6eaf7f7b9a7b074
NoobZik <noobzik@pm.me>
2022-04-14 16:13:17 +0200

    Fixed typo

--- kafka-stream/src/main/scala/org/esgi/project/api/models/StatResponse.scala
+++ kafka-stream/src/main/scala/org/esgi/project/api/models/StatResponse.scala
@@ -2,12 +2,12 @@ package org.esgi.project.api.models
 
 import play.api.libs.json.{Json, OFormat}
 
-case class Stat(
+case class StatResponse(
                  start_only: Long,
                  half : Long,
                  full: Long
                )
 
 object StatResponse {
-  implicit val format: OFormat[Stat] = Json.format[Stat]
+  implicit val format: OFormat[StatResponse] = Json.format[StatResponse]
 }
