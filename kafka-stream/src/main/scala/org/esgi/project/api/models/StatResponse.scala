d8fd10a564c22588d5abf6513de535d03edb10c2
NoobZik <noobzik@pm.me>
2022-04-15 12:06:06 +0200

    Fixed typos

--- kafka-stream/src/main/scala/org/esgi/project/api/models/StatResponse.scala
+++ kafka-stream/src/main/scala/org/esgi/project/api/models/StatResponse.scala
@@ -6,8 +6,19 @@ case class StatResponse(
                  start_only: Long,
                  half : Long,
                  full: Long
-               )
+               ){
+  def accumulator(start_only: Long, half: Long, full: Long): StatResponse = {
+    this.copy(
+      start_only = this.start_only + start_only,
+      half = this.half + half,
+      full = this.full + full
+    )
+  }
+}
 
 object StatResponse {
   implicit val format: OFormat[StatResponse] = Json.format[StatResponse]
+
+  def empty : StatResponse = StatResponse(start_only = 0, half = 0, full = 0)
+
 }
